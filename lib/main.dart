ab26a838f83f3f072e8a307ae27cb65a1833095d
hp <fadysamuel18@gmail.com>
2024-03-03 23:10:47 +0200

    login screen register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:to_do_list/home_screen.dart';
 
-void main(){
-  runApp(MyApp());
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_list/auth/regist/registeration_tap.dart';
+import 'package:to_do_list/home/home_screen.dart';
+import 'package:to_do_list/login_screen.dart';
+import 'package:to_do_list/my_theme.dart';
+import 'package:to_do_list/provider/provider.dart';
+
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  await FirebaseFirestore.instance.disableNetwork();
+
+  runApp(ChangeNotifierProvider(
+    create: (context) => ListProvider(),
+      child: MyApp()));
 }
 
 
@@ -11,11 +28,15 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: HomeScreen.routeName ,
+      initialRoute: LoginTap.routeName ,
       routes: {
         HomeScreen.routeName :(context) => HomeScreen(),
+        RegisterTap.routeName : (context) => RegisterTap(),
+        LoginTap.routeName : (context) => LoginTap(),
+
 
       },
+      theme: MyTheme.lightTheme,
     );
   }
 }
